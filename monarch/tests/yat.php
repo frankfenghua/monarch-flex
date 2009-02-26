bb2106601491c727b1ed2a64b1d18e442c47691c
ryan <ryan@ff7643bc-2584-11de-b09f-0fcc949ee51a>
2009-02-26 02:16:37 +0000

    regexes is a standalone AIR app that does realtime regular expression matching. This code will eventually be merged into the main GUI branch.

--- monarch/tests/yat.php
+++
@@ -1,8 +0,0 @@
-<?php
-  // Yet another test
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
-$db = mysql_connect('localhost', 'ryan', 'adobe') or die('Could not connect to the database');
-
-echo "Testing";
-?>
