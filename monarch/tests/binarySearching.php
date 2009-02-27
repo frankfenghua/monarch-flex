6a4a67021120c11411fe818bc9b369b464a5c429
ryan <ryan@ff7643bc-2584-11de-b09f-0fcc949ee51a>
2009-02-27 04:53:58 +0000

    updates to binarySearching.php
    -ryan lin

--- monarch/tests/binarySearching.php
+++ monarch/tests/binarySearching.php
@@ -27,9 +27,23 @@
 		'horriblemisspeilling', 'christ', 'damn', 'dam', 'dude', 'zany', 'fork', 
 		'process', 'whore', 'bin', 'lesbo', 'god', 'hemeroid', 'killer', 'kill',
 		'animal', 'yellow', 'blue', 'green', 'greedy', 'algorithm', 'barfinyourface',
-		'nigarrofigaro');
-		
-	foreach($findThese as $findThis)
-		printf('<h3>index where <font color="red">%s</font> is located: <font color="green">%d</font></h3>', $findThis, $binary->findIndex($findThis));
+		'nigarrofigaro', 'hoe', 'hot', 'dog', 'mustard', 'ketchup', 'bong', 'puneet',
+		'vera', 'adobe', 'robot', 'hardcoded', 'hard', 'soft', 'cunt', 'blows', 
+		'hits', 'punches', 'punching', 'kick', 'kicker', 'kickers', 'kicks', 'kicked',
+		'medicinal', 'marijuana', 'alcohol', 'abuse', 'triple', 'a', 'jail', 'slap',
+		'womanizer', 'femenist', 'zeal', 'zoo', 'june', 'april');
+	
+	if($_GET['binary'] == 'true')
+	{	
+		foreach($findThese as $findThis)
+			printf('<h3>index where <font color="red">%s</font> is located: <font color="green">%d</font></h3>', $findThis, $binary->findIndex($findThis));
+	}
+	else if($_GET['binary'] == 'false')
+	{
+		foreach($findThese as $findThis)
+			printf('<h3>index where <font color="red">%s</font> is located: <font color="green">%d</font></h3>', $findThis, array_search($findThis, $englishDictionary));
+	}
+	else
+		die('usage: binarySearching.php?binary=true || binarySearching.php?binary=false');
 
 ?>
