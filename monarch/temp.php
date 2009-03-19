31a240987878c07576acbc1da00bcbfd092ee4e9
ryan <ryan@ff7643bc-2584-11de-b09f-0fcc949ee51a>
2009-03-19 17:16:57 +0000

    * edit community - List componenet placeholder for keywords and websites. * more tooltips. * add website is in TitleWindow instead of Window for consistent style. * fixed broken images in regex editor
    - ryan

--- monarch/temp.php
+++ monarch/temp.php
@@ -1,16 +1,31 @@
 <?php
 
-	require('BinarySearch.php');
-	
-	$arr = array('za', 'cd', 'ab', 'orp');
-	
-	$bin = new BinarySearch($arr, false);
-	
-	echo $bin->findIndex('aa');
-	echo $bin->findIndex('zz');
-	echo $bin->findIndex('cd');
-	echo $bin->findIndex('za');
-	echo $bin->findIndex('ab');
-	
+$html = file_get_contents('http://www.cnn.com/2009/POLITICS/03/18/aig.bonuses.congress/index.html');
+
+/*
+
+<b>(CNN)</b> -- Senate Banking committee Chairman Christopher Dodd told CNN Wednesday that he was responsible for language added to the federal stimulus bill to make sure that already-existing contracts for bonuses at companies receiving federal bailout money were honored.</p> <!--startclickprintexclude-->
+
+
+
+
+
+
+*/
+
+// preg_match_all('/<h3>\n<a href="(.*?)"/', $html, $res);
+
+// preg_match_all('/<h3>\n<a href=".*?">(.*?)</', $html, $res);
+
+//preg_match_all('/<p class="cnnAttribution">.*? (.*?) contributed/', $html, $res);
+
+// preg_match_all('/([0-9]{4}\/[0-9]{2}\/[0-9]{2})\//', $html, $res);
+
+preg_match_all('/<div id="cnnHighLightTrigger">(.*?)<\/div>/s', $html, $res);
+
+print_r($res);
+
+
+
 ?>
 
