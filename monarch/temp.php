5fcd497b2f74819c6920d350115c6ffd1096724d
ryan <ryan@ff7643bc-2584-11de-b09f-0fcc949ee51a>
2009-03-26 18:14:46 +0000

    * disabled "newest" button in group selection because that's what is already selected.
    - ryan

--- monarch/temp.php
+++ monarch/temp.php
@@ -1,32 +1,13 @@
 <?php
 
-/*
+include('database/Database.php');
+$db = new Database('master');
 
-/<div class="comment_time">[\n][\s]+said[\n][\s]+([0-9]+ [a-zA-Z]+[\n][\s]+ago)/
+$q = 'update badwords set word = "suicide" where word = ' . mysql_real_escape_string('0; drop table badwords');
 
+echo $q;
 
-
-
-*/
-
-$html = file_get_contents('http://www.designbyhumans.com/forums/show/405884');
-
-
-// preg_match_all('/<div class="forum-post-title">[\n][\s]+<a href="(.*?)"/', $html, $ret);
-
-// preg_match_all('/<div class="forum-post-title">[\n][\s]+<a href="(?:.*?)">(.*?)</', $html, $ret);
-
-// preg_match_all('/posted[\s]+[\n][\s]+(?:about)? (.*?)[\n]/', $html, $ret);
-
-preg_match_all('/posted[\s]+[\n][\s]+(?:about)? (.*?)[\n]/', $html, $ret);
-
-
-echo '<pre>';
-print_r($ret);
-echo '</pre>';
-
-
-// echo date('m j y', strtotime($ret[1][1]));
+$db->query($q);
 
 ?>
 
