8bb76027079269270ab8cde978307c75b75e8742
ryan <ryan@ff7643bc-2584-11de-b09f-0fcc949ee51a>
2009-04-07 20:05:10 +0000

    Show community icon in edit community page. Also refresh that image when a new icon is uploaded.
    - ryan

--- monarch/imageUpload.php
+++ monarch/imageUpload.php
@@ -4,10 +4,13 @@ $newfilename = $_POST['id'] . '.jpg';
 move_uploaded_file($_FILES['Filedata']['tmp_name'], './tmp/' . $_FILES['Filedata']['name']);
 rename('./tmp/' . $_FILES['Filedata']['name'], './images/' . $newfilename);
 
+
 echo 'Successfully Uploaded';
 
 require_once('PhotoManip.php');
 $photoManip = new PhotoManip('./images/' . $newfilename);
 $photoManip->thumbnail(64, 64);
+
+
 
 ?>
