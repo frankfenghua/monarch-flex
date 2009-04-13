f6b7fbb732af2e0164a728bb4b47f2b56af75d6b
tokyotech <tokyotech@gmail.com>
2009-04-13 06:23:57 +0000

    trying to convert time with HTML entities into Unix timestamp - fail

--- monarch-server/temp.php
+++ monarch-server/temp.php
@@ -1,29 +1,23 @@
 <?php
 
-/*
-
-/<span class="fontsize2 author">\n[\s]{2}<span style="color: #00681C;">(.*?)<\/span>/
-<span class="fontsize2 author">
-
-  <span style="color: #790619;">Major Debacle</span>
-
-
-
-
-
-
-
-
-*/
-
 $html = file_get_contents('http://groups.google.com/group/alt.politics.usa/browse_thread/thread/419b783eccd25a13#');
 
-preg_match_all('/<span class="fontsize0">&nbsp;<span class="(?:.*?)" id="oh_l">More options<\/span><\/span>\n  <span class="fontsize2">\n  (.*?)\n/', $html, $res);
+preg_match_all('/<span class="fontsize0">&nbsp;<span class="(?:.*?)" id="oh_l">More options<\/span><\/span>\n  <span class="fontsize2">\n  (.*?)\n/', $html, $result);
 
-echo html_entity_decode('&lt;b&gt;bo&nbsp;ld&lt;/b&gt;');
 
-//print_r($res);
+echo '$result[1][0] = ' . $result[1][0];
 
-// print_r(strtotime($res[1][0]));
+echo '<br />';
+
+echo 'html_entity_decode($result[1][0]) = ' . html_entity_decode($result[1][0]); 
+
+echo '<br />';
+
+echo 'strtotime(html_entity_decode($result[1][0])) = ' . strtotime(html_entity_decode($result[1][0]));
+
+echo '<br />';
+
+echo 'strtotime("Apr 10, 8:19 am") = ' . strtotime("Apr 10, 8:19 am");
+
 
 ?>
