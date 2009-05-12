e47a1fd57539a5a57b95d9acc9667436289129c8
tokyotech <tokyotech@gmail.com>
2009-05-12 04:55:05 +0000

    put quasi-dynamic images into monarch/Client

--- monarch-server/Client/imageUpload.php
+++ monarch-server/Client/imageUpload.php
@@ -2,15 +2,12 @@
 
 $newfilename = $_POST['id'] . '.jpg';
 move_uploaded_file($_FILES['Filedata']['tmp_name'], './tmp/' . $_FILES['Filedata']['name']);
-rename('./tmp/' . $_FILES['Filedata']['name'], './images/' . $newfilename);
-
-
-echo 'Successfully Uploaded';
+rename('./tmp/' . $_FILES['Filedata']['name'], '../../images/' . $newfilename);
 
 require_once('PhotoManip.php');
-$photoManip = new PhotoManip('./images/' . $newfilename);
+$photoManip = new PhotoManip('../../images/' . $newfilename);
 $photoManip->thumbnail(64, 64);
 
-
+echo 'Successfully Uploaded';
 
 ?>
