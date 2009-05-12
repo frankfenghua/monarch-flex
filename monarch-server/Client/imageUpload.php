80a6d699f80e6e40a13116a3a93667ea4cf918c2
tokyotech <tokyotech@gmail.com>
2009-05-12 04:59:46 +0000

    attempt to fix image upload directory error

--- monarch-server/Client/imageUpload.php
+++ monarch-server/Client/imageUpload.php
@@ -1,13 +1,20 @@
 <?php
 
+// create the folder for storing thumbnails
+if(!file_exists('../../images/')
+	mkdir('../../images');
+
+// TODO: Andrew, what's all this shit?
 $newfilename = $_POST['id'] . '.jpg';
 move_uploaded_file($_FILES['Filedata']['tmp_name'], './tmp/' . $_FILES['Filedata']['name']);
 rename('./tmp/' . $_FILES['Filedata']['name'], '../../images/' . $newfilename);
 
+// take the uploaded image and make a thumbnail out of it
 require_once('PhotoManip.php');
 $photoManip = new PhotoManip('../../images/' . $newfilename);
 $photoManip->thumbnail(64, 64);
 
+// notify GUI
 echo 'Successfully Uploaded';
 
 ?>
