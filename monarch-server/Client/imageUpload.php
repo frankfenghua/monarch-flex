bae1f7ff232b2c3c1ea01e20b5a32900c78c1b8a
tokyotech <tokyotech@gmail.com>
2009-05-12 05:09:01 +0000

    try fix image upload again

--- monarch-server/Client/imageUpload.php
+++ monarch-server/Client/imageUpload.php
@@ -9,12 +9,12 @@ $newfilename = $_POST['id'] . '.jpg';
 move_uploaded_file($_FILES['Filedata']['tmp_name'], './tmp/' . $_FILES['Filedata']['name']);
 rename('./tmp/' . $_FILES['Filedata']['name'], '../../images/' . $newfilename);
 
+// notify GUI
+echo 'Successfully Uploaded';
+
 // take the uploaded image and make a thumbnail out of it
 require_once('PhotoManip.php');
 $photoManip = new PhotoManip('../../images/' . $newfilename);
 $photoManip->thumbnail(64, 64);
-
-// notify GUI
-echo 'Successfully Uploaded';
 
 ?>
